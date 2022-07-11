6a86e9e6150a264af91accef41169450a84f4bec
python-gurux-1 <108383232+python-gurux-1@users.noreply.github.com>
2022-07-11 07:16:19 +0300

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,19 +1,19 @@
-Izzatullo: 5436799209:AAFhzbJ-FZo_wQasBioDYr1dPrWPDXcrvKI
+Izzatullo: 5436799209:AAFhzbJ-FZo_wQasBioDYr1dPrWPDXcrvKI ,  hshshshdhheieiqkdbot
  
-Boburbek: 5475391696:AAHipBKXSjQ5MvtkcbbqwIJKz8AnGxep9EM
+Boburbek: 5475391696:AAHipBKXSjQ5MvtkcbbqwIJKz8AnGxep9EM , asdtjwerifgsadhfkjhjertgbot
   
 Sarvarbek: 5420773758:AAGbccjfK5aRj70VKBqBbdf9jNN81f72U2Q , dfhrtydfgbxcvhyethjdfgbnbot
   
   
 Fathullo: 5565268782:AAEnAGbcMfhtT3OjIRh4UwVM6EBCcvSjFMY ,  sergsbrthsfbscvgergsadfbot
   
-Asadbek: 5492501751:AAGu5aJ_KAco3gNyztulifUwRe-cDcFFYGM
+Asadbek: 5492501751:AAGu5aJ_KAco3gNyztulifUwRe-cDcFFYGM , Jebzbakkqdbot
   
-Mubinahon: 5414868479:AAHPxZHBOx8gOqNitRhsOkmIpZQvZn7Wn5o
+Mubinahon: 5414868479:AAHPxZHBOx8gOqNitRhsOkmIpZQvZn7Wn5o , Bsgakaorbdisueebot
 
   
-Ikromiddin: 5306619798:AAHsgMxh6rpx0Bvdkyjxz61Xt2SA4Pm1Zuo
+Ikromiddin: 5306619798:AAHsgMxh6rpx0Bvdkyjxz61Xt2SA4Pm1Zuo , Tshskaksfbot
 
 Bekzod: 5454916812:AAHCjRyroNtN10RPL9qdC5Il-UEbID0cVfo , Uwoqlsnbxbxjsowbfhdksbot
 
-menga: 1919645958:AAGvkfGr-Q42CVB1tzh9G0bjj6VA4IPMba4
+menga: 1919645958:AAGvkfGr-Q42CVB1tzh9G0bjj6VA4IPMba4 , FayzulloTestBot
