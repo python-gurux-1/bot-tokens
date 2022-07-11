90f9a497077e0c56cd5fbdd889d0de00ee9f4883
python-gurux-1 <108383232+python-gurux-1@users.noreply.github.com>
2022-07-11 07:07:30 +0300

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -2,10 +2,10 @@ Izzatullo: 5436799209:AAFhzbJ-FZo_wQasBioDYr1dPrWPDXcrvKI
  
 Boburbek: 5475391696:AAHipBKXSjQ5MvtkcbbqwIJKz8AnGxep9EM
   
-Sarvarbek: 5420773758:AAGbccjfK5aRj70VKBqBbdf9jNN81f72U2Q
+Sarvarbek: 5420773758:AAGbccjfK5aRj70VKBqBbdf9jNN81f72U2Q , dfhrtydfgbxcvhyethjdfgbnbot
   
   
-Fathullo: 5565268782:AAEnAGbcMfhtT3OjIRh4UwVM6EBCcvSjFMY
+Fathullo: 5565268782:AAEnAGbcMfhtT3OjIRh4UwVM6EBCcvSjFMY ,  sergsbrthsfbscvgergsadfbot
   
 Asadbek: 5492501751:AAGu5aJ_KAco3gNyztulifUwRe-cDcFFYGM
   
@@ -14,6 +14,6 @@ Mubinahon: 5414868479:AAHPxZHBOx8gOqNitRhsOkmIpZQvZn7Wn5o
   
 Ikromiddin: 5306619798:AAHsgMxh6rpx0Bvdkyjxz61Xt2SA4Pm1Zuo
 
-Bekzod: 5454916812:AAHCjRyroNtN10RPL9qdC5Il-UEbID0cVfo
+Bekzod: 5454916812:AAHCjRyroNtN10RPL9qdC5Il-UEbID0cVfo , Uwoqlsnbxbxjsowbfhdksbot
 
 menga: 1919645958:AAGvkfGr-Q42CVB1tzh9G0bjj6VA4IPMba4
